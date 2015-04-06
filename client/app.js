89ab2f69eb084ba2b9b4b9de97d15b7890ea8ce4
Vizhanyo Mihaly <vizhanyo.mihaly@gmail.com>
2015-04-05 18:15:13 -0700

    express and react build added
    - major directory restructuring:
    -- static content moved to /client (most of the app)
    -- third party libraries moved to /client/lib
    - express server added

---
+++ client/app.js
@@ -0,0 +1,12 @@
+var master;
+$.ajax({ url: "master.json", dataType: "json" })
+.done(function( data ) {
+	master = data;
+	React.render(
+		React.createElement("h1", null, master.species[3].name),
+		document.getElementById('example')
+		);
+})
+.fail(function( request, errorMsg, exception ) {
+	alert('fail ' + errorMsg);
+});
---
+++ client/app.jsx
@@ -0,0 +1,12 @@
+var master;
+$.ajax({ url: "master.json", dataType: "json" })
+.done(function( data ) {
+	master = data;
+	React.render(
+		<h1>{master.species[3].name}</h1>,
+		document.getElementById('example')
+		);
+})
+.fail(function( request, errorMsg, exception ) {
+	alert('fail ' + errorMsg);
+});
