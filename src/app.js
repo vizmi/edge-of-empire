2899f15c6a209bfafdd1e5cbe544db1024f5f34d
Vizhanyo Mihaly <vizhanyo.mihaly@gmail.com>
2015-04-06 20:38:33 -0700

    project reorganization

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
-React.render(
-  <h1>Hello Misi!</h1>,
-  document.getElementById('example')
-);
+var master;
+$.ajax({ url: "data/master.json", dataType: "json" })
+.done(function( data ) {
+	master = data;
+	React.render(
+		<h1>{master.species[3].name}</h1>,
+		document.getElementById('example')
+		);
+})
+.fail(function( request, errorMsg, exception ) {
+	alert('fail ' + errorMsg);
+});
--- static/build/app.js
+++ static/build/app.js
@@ -1,4 +1,12 @@
-React.render(
-  React.createElement("h1", null, "Hello Misi!"),
-  document.getElementById('example')
-);
+var master;
+$.ajax({ url: "data/master.json", dataType: "json" })
+.done(function( data ) {
+	master = data;
+	React.render(
+		React.createElement("h1", null, master.species[3].name),
+		document.getElementById('example')
+		);
+})
+.fail(function( request, errorMsg, exception ) {
+	alert('fail ' + errorMsg);
+});
